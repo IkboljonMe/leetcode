9ccd618d2d420da72b7dba4f398388ad3debcb39
Ikboljon Abdurasulov <ikboljon@radcode.co>
2023-03-21 11:46:23 +0100

    unique occurences

---
+++ JavaScript/50days_marathon/1_day/UniqueOccurrences/index.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+//LINK: https://leetcode.com/problems/unique-number-of-occurrences/
+var uniqueOccurrences = function (arr) {
+  const deduplicatedSet = new Set(arr);
+  const deduplicatedArray = [...deduplicatedSet];
+  const occurrences = [];
+
+  for (let i = 0; i < deduplicatedArray.length; i++) {
+    let specificOccurence = 0;
+    arr.map((el) => {
+      if (el == deduplicatedArray[i]) {
+        specificOccurence++;
+      }
+    });
+    occurrences.push(specificOccurence);
+  }
+  const checkingDuplicationSet = new Set(occurrences);
+  const checkingDuplicationSetArr = [...checkingDuplicationSet];
+
+  if (occurrences.length == checkingDuplicationSetArr.length) {
+    return true;
+  } else {
+    return false;
+  }
+};
