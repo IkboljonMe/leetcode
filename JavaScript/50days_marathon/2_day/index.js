6f1d9365e145835936156aa76b18118b29aa8cc6
Ikboljon Abdurasulov <ikboljon@radcode.co>
2023-03-22 13:03:51 +0100

    plus one

---
+++ JavaScript/50days_marathon/2_day/index.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+//LINK: https://leetcode.com/problems/plus-one/
+var plusOne = function (digits) {
+  let index = digits.length - 1;
+  while (index >= 0 && digits[index] === 9) {
+    digits[index] = 0;
+    index--;
+  }
+  if (index == -1) {
+    digits.unshift(1);
+  } else {
+    digits[index]++;
+  }
+  return digits;
+};
