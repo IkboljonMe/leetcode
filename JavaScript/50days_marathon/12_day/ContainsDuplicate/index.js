5ac315b5b0dc74ab3764f0b3e257a1ed407766b4
Ikboljon Abdurasulov <ikboljon@radcode.co>
2023-04-05 01:25:02 +0200

    added -- link

--- JavaScript/50days_marathon/12_day/ContainsDuplicate/index.js
+++ JavaScript/50days_marathon/12_day/ContainsDuplicate/index.js
@@ -2,6 +2,7 @@
  * @param {number[]} nums
  * @return {boolean}
  */
+//LINK: https://leetcode.com/problems/contains-duplicate/solutions/3380086/4-line-solutionjavascripteasy-explanation9971-beats/
 var containsDuplicate = function (nums) {
   const set = new Set(nums);
   if (set.size == nums.length) {
