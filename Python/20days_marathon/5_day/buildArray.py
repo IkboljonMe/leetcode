4af53dd7358999538c6ce8932b83c9be72aa3cfd
Ikboljon Abdurasulov <ikboljon@radcode.co>
2023-05-06 00:17:33 +0200

    better aproach

--- Python/20days_marathon/5_day/buildArray.py
+++ Python/20days_marathon/5_day/buildArray.py
@@ -3,7 +3,9 @@
 # MEMORY: O(n)
 class Solution:
     def buildArray(self, nums):
+        n = len(nums)
         ans = []
-        for i in range(len(nums)):
-            ans.append(nums[nums[i]])
+        for i in range(n):
+            if nums[i] < n:
+                ans.append(nums[nums[i]])
         return ans
