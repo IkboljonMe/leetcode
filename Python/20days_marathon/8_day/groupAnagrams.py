90b425599f8b097d39174e57d95fad474c3e18c2
Ikboljon Abdurasulov <ikboljon@radcode.co>
2023-05-08 15:14:38 +0200

    links, comments added

--- Python/20days_marathon/8_day/groupAnagrams.py
+++ Python/20days_marathon/8_day/groupAnagrams.py
@@ -1,3 +1,5 @@
+# LINK: https://leetcode.com/problems/group-anagrams/
+# COMPLEXITY: The time complexity is O(N * K * log K), and the space complexity is O(N * K), where N is the number of strings in the input list strs, and K is the maximum length of a string in strs.
 import collections
 class Solution:
     def groupAnagrams(self, strs):
